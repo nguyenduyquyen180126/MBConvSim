ea3d33c26c68a89aaf776da42bce95ff45b4fbd3
NguyenQuyen <nguyenduyquyen18012006@gmail.com>
2026-03-01 10:20:57 +0700

    first commit

---
+++ bram.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+int8_t IFM_BRAM[][16];
+int8_t W0_BRAM[][16];
+int8_t W1_BRAM[][16];
+int8_t W2_BRAM[][16];
+int8_t W3_BRAM[][16];
+int8_t W4_BRAM[][16];
+int8_t W5_BRAM[][16];
+int8_t W6_BRAM[][16];
+int8_t W7_BRAM[][16];
+int8_t W8_BRAM[][16];
+int8_t W9_BRAM[][16];
+int8_t W10_BRAM[][16];
+int8_t W11_BRAM[][16];
+int8_t W12_BRAM[][16];
+int8_t W13_BRAM[][16];
+int8_t W14_BRAM[][16];
+int8_t W15_BRAM[][16];
+
+void load_bram()
+
+int main(){
+
+}
